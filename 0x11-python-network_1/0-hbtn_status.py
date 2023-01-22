463d801857b0df78bafdf47e0730ebbe4b363d36
Terenceonye <onyeweketerence@gmail.com>
2023-01-22 08:57:53 -0800

    another

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 
-"""Fetches https://intranet.hbtn.io/status"""
+"""A script that
 
-import urllib.request
+- fetches https://alx-intranet.hbtn.io/status.
+
+- uses urlib package
+
+"""
 
 
 
 
 
-def fetcher():
+if __name__ == '__main__':
 
-        """fetcher"""
+        import urllib.request
 
-            with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
 
-                        html = response.read()
+
+            with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+
+                        content = res.read()
 
                                 print("Body response:")
 
-                                        print("\t- type: {}".format(type(html)))
+                                        print("\t- type: {}".format(type(content)))
 
-                                                print("\t- content: {}".format(html))
+                                                print("\t- content: {}".format(content))
 
-                                                        print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
-
-
-                                                        if __name__ == "__main__":
-
-                                                                fetcher()
+                                                        print("\t- utf8 content: {}".format(content.decode('utf-8')))
